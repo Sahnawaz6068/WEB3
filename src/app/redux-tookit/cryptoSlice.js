3f2d83d57418b7dde965ddb77e55f1f3c0762319
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-11 13:41:37 +0530

    Added async fetch call the api

--- src/app/redux-tookit/cryptoSlice.js
+++ src/app/redux-tookit/cryptoSlice.js
@@ -1,25 +1,48 @@
-'use client'
- 
-const initialState={
-    
-    favorites:[],
-    isLoading:false,
-    error:null,
-}
+"use client";
 
-const cryptoSlice=createSlice({
-    name:'crypto',
-    initialState,
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-    reducers:{
-        addFavorite:(state,action)=>{
-            state.favorites.push();
-        },
+const initialState = {
+    coin:[],
+  favorites: [],
+  isLoading: false,
+  error: null,
+};
+//____________________________________________________________________________________
 
-        removeFavorite:(state,action)=>{
-            state.favorites.filter(
-                //wo sara coin jo 
-            )
-        }
-    }
+//Data fetching  Thunk 
+export const fetchCoins=createAsyncThunk("Crypto", async()=>{
+    const reponse=await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    const data=await reponse.json();
+    return data;
 })
+
+
+const cryptoSlice = createSlice({
+  name: "crypto",
+  initialState,
+
+  reducers: {
+    //Action that are sync
+    addFavorite: (state, action) => {
+      state.favorites.push(action.payload);
+    },
+
+    removeFavorite: (state, action) => {
+      state.favorites
+        .filter (coin=>coin.id !== action.payload.id);
+        //wo sara coin alawa uske jo remove huaa
+       
+    },
+  },
+  //async actions
+  extraReducers:(builder)=>{ 
+    //Manage of promise state
+    builder.addCase(fetchCoins.pending,    )
+    builder.addCase(fetchCoins.fulfilled,    )
+    builder.addCase(fetchCoins.rejected,    )
+  }
+
+});
+
+export const {addFavorite,removeFavorite}=Slice.actions;
